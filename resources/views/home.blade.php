1354c9ffbc084520a96fcbbde87a9df82f695f3b
Pitchayakit <pitchayakit.p@gmail.com>
2018-04-10 16:43:50 +0700

    css change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,24 +5,23 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Thanks survey</div>
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success">
                             {{ session('status') }}
                         </div>
                     @endif
-                    <h4>Your friends</h4>  
                     <form action="/home" method="POST">
                         {{ csrf_field() }}
                         @foreach ($following_users as $user)
-                            <p>{{ $user->following->name }}</p>
+                            <div>{{ $user->following->name }}</div>
                             <input type="radio" name="{{ $user->following->id }}" value="1"> 1
                             <input type="radio" name="{{ $user->following->id }}" value="2"> 2
                             <input type="radio" name="{{ $user->following->id }}" value="3"> 3
                             <input type="radio" name="{{ $user->following->id }}" value="4"> 4
                         @endforeach
-                        <div><input type="submit" value="Submit"></div>
+                        <div><input type="submit" class="btn btn-primary" value="Submit"></div>
                     </form>
                 </div>
             </div>
