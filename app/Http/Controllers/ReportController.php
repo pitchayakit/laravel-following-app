27bd710fa4db9c7d5682415452f5f0054ec90986
Pitchayakit <pitchayakit.p@gmail.com>
2018-04-27 16:47:34 +0700

    bug report fixed

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -36,27 +36,28 @@ class reportController extends Controller
                 $user_name_row[$j] = 0;
             }
             $user_votes = $user->votes;
-            $date = $user_votes[0]->created_at;
-            foreach ($user_votes as $vote) {
-                if($vote->created_at != $date) {
-                    $user_name_row_date = $user_name_row;
-                    array_push($user_name_row_date, $date);
-                    fputcsv($file,$user_name_row_date);
-                    for($j = 1; $j < count($users_name_row); $j++){
-                        $user_name_row[$j] = 0;
-                    }
-                    $date = $vote->created_at;
-                } 
-                for($j = 1; $j < count($users_name_row); $j++) {
-                    if( (User::find($vote->voted_id)->name === $users_name_row[$j]) ){
-                        $user_name_row[$j] = $vote->point;
-                    }
-                }  
+            if(!empty($user_votes[0]->created_at)){
+                $date = $user_votes[0]->created_at;
+                foreach ($user_votes as $vote) {
+                    if($vote->created_at != $date) {
+                        $user_name_row_date = $user_name_row;
+                        array_push($user_name_row_date, $date);
+                        fputcsv($file,$user_name_row_date);
+                        for($j = 1; $j < count($users_name_row); $j++){
+                            $user_name_row[$j] = 0;
+                        }
+                        $date = $vote->created_at;
+                    } 
+                    for($j = 1; $j < count($users_name_row); $j++) {
+                        if( (User::find($vote->voted_id)->name === $users_name_row[$j]) ){
+                            $user_name_row[$j] = $vote->point;
+                        }
+                    }  
+                }
+                $user_name_row_date = $user_name_row;
+                array_push($user_name_row_date,$user_votes[count($user_votes)-1]->created_at);
+                fputcsv($file,$user_name_row_date);
             }
-            $user_name_row_date = $user_name_row;
-            array_push($user_name_row_date,$user_votes[count($user_votes)-1]->created_at);
-            fputcsv($file,$user_name_row_date);
-            
         }
 
         fclose($file);
