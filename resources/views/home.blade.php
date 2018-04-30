7d440528b4bd90f286ba8cf9e2458061c740bfc4
Pitchayakit <pitchayakit.p@gmail.com>
2018-04-30 16:01:36 +0700

    add img on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,10 +16,14 @@
                         {{ csrf_field() }}
                         @foreach ($following_users as $user)
                             <div>{{ $user->following->name }}</div>
-                            <input type="radio" name="{{ $user->following->id }}" value="1" required> 1
-                            <input type="radio" name="{{ $user->following->id }}" value="2"> 2
-                            <input type="radio" name="{{ $user->following->id }}" value="3"> 3
-                            <input type="radio" name="{{ $user->following->id }}" value="4"> 4
+                            <img src="https://graph.facebook.com/v2.10/{{ $user->following->socialAccount->provider_user_id }}/picture?type=large">
+                            <div>
+                                <input type="radio" name="{{ $user->following->id }}" value="1" required> 1
+                                <input type="radio" name="{{ $user->following->id }}" value="2"> 2
+                                <input type="radio" name="{{ $user->following->id }}" value="3"> 3
+                                <input type="radio" name="{{ $user->following->id }}" value="4"> 4
+                            </div>
+                            <hr />
                         @endforeach
                         <div><input type="submit" class="btn btn-primary" value="Submit"></div>
                     </form>
